13767efbb3063ccea687b6693cae45e43e67772b
Matthew Lu <pamalu.225@gmail.com>
2026-01-14 21:35:44 -0500

    team page

--- src/pages/team.jsx
+++ src/pages/team.jsx
@@ -1,15 +1,47 @@
 import Navbar from '../Navbar';
 import './team.css';
 
+import matthew from '../assets/matthew_p_p.jpeg';
+import kai from '../assets/kai_p_p.jpg';
+import delaney from '../assets/delaney_p_p.jpg';
+import tommy from '../assets/tommy_p_p.jpg';
+
 const Team = () => {
   return (
-    <div className='team-container'>
-      <Navbar />  
-      <div className='team-header'>
-        <h1>Team Page Coming Soon!</h1>
-      </div>      
+    <div className="team-container">
+      <Navbar />
+
+      <div className="team-header">
+        <h1>Meet Our Team of Dedicated Student Tutors</h1>
+      </div>
+
+      <div className="team-grid">
+        <div className="team-member">
+          <img src={matthew} alt="Matthew Lu" className="team-photo matthew-photo" />
+          <p className="team-name">Matthew Lu</p>
+          <p className="team-subjects">Computer Science • Statistics • Economics</p>
+        </div>
+
+        <div className="team-member">
+          <img src={kai} alt="Kai Dehus" className="team-photo kai-photo" />
+          <p className="team-name">Kai Dehus</p>
+          <p className="team-subjects">Engineering • Calculus</p>
+        </div>
+
+        <div className="team-member">
+          <img src={delaney} alt="Delaney Asbury" className="team-photo delaney-photo" />
+          <p className="team-name">Delaney Asbury</p>
+          <p className="team-subjects">Biology • Chemistry • Neuroscience</p>
+        </div>
+
+        <div className="team-member">
+          <img src={tommy} alt="Thomas Selwood" className="team-photo tommy-photo" />
+          <p className="team-name">Thomas Selwood</p>
+          <p className="team-subjects">Biology • Chemistry</p>
+        </div>
+      </div>
     </div>
-  )
+  );
 };
 
 export default Team;
