2b908ffdcabdf74696ec101461196c8a0c3bf678
Matthew Lu <pamalu.225@gmail.com>
2026-01-17 18:38:49 -0500

    mobile navbar

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -4,16 +4,46 @@ import { useState } from 'react';
 const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
 
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
-    <nav className="navbar">
-      <div className="navbar-left">
-        <a href="/home" className="logo">
-          {/* <img src="src/assets/pantherpartners.png" alt="panther partners"/> */}
-          Panther Partners
-        </a>
-      </div>
-      <div className="navbar-center">
-        <ul className="nav-links">
+    <div>
+      <nav className="navbar">
+        <div className="navbar-left">
+          <a href="/home" className="logo">
+            {/* <img src="src/assets/pantherpartners.png" alt="panther partners"/> */}
+            Panther Partners
+          </a>
+        </div>
+        <div className="navbar-center">
+          <ul className="nav-links">
+            <li>
+              <a href="/about">About Us</a>
+            </li>
+            <li>
+              <a href="/team">Team</a>
+            </li>
+            <li>
+              <a href="/pricing">Pricing</a>
+            </li>
+          </ul>
+        </div>
+        <div className="navbar-right">
+          <a href="/contact">Contact Us</a>
+        </div>
+        <div className="hamburger" onClick={toggleMenu}>
+            <span className="bar"></span>
+            <span className="bar"></span>
+            <span className="bar"></span>
+        </div>
+      </nav>
+      <div className={`off-screen-menu ${isOpen ? 'off-screen-menu-open' : ''}`}>
+        <div className='close-tab' onClick={toggleMenu}>
+          <span className="close-bar"></span>
+        </div>
+        <ul>
           <li>
             <a href="/about">About Us</a>
           </li>
@@ -23,17 +53,12 @@ const Navbar = () => {
           <li>
             <a href="/pricing">Pricing</a>
           </li>
+          <li>
+            <a href="/contact">Contact Us</a>
+          </li>
         </ul>
       </div>
-      <div className="navbar-right">
-        <a href="/contact">Contact Us</a>
-      </div>
-      <div className="hamburger" onClick={() => setIsOpen(!isOpen)}>
-          <span className="bar"></span>
-          <span className="bar"></span>
-          <span className="bar"></span>
-      </div>
-    </nav>
+    </div>
   );
 };
 
--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -10,16 +10,28 @@ const About = () => {
         <h1>Our dream is building a smarter way to learn—by students, for students</h1>
         <p>We’re a student-founded tutoring organization built to support the academic success of University of Pittsburgh students. Having navigated the same classes, professors, and challenges ourselves, we understand what it takes to succeed at Pitt.
 Our tutors are high-achieving Pitt students who combine strong subject knowledge with a practical, approachable teaching style. We focus on clarity, confidence, and strategies that actually work—whether that means mastering difficult concepts, preparing for exams, or staying on track throughout the semester.
-We believe learning works best when it’s collaborative, supportive, and tailored to each student’s goals. Our mission is to make high-quality, peer-led tutoring accessible and effective, helping students not just get through their courses, but truly understand them.</p>
+We hope to not only help our students achieve academic success, but to provide them with advice on planning for a successful future and adjusting to college life. Our mission is to make high-quality, peer-led tutoring accessible and effective, helping students not just get through their courses, but truly understand them.</p>
       </div>
       <div className="about-right">
         <img src={studying} alt="Studying" className="about-image" />
         <div className="about-stats"> 
           <div className="about-grid">  
-            <div className="about-grid-item">X+ Students Tutored</div> 
-            <div className="about-grid-item">X+ Sessions Completed</div> 
-            <div className="about-grid-item">X+ Courses Covered</div> 
-            <div className="about-grid-item">X+ Average Tutor GPA</div> 
+            <div className="about-grid-item">
+              <h1>30+</h1>
+              <p>Students Tutored</p>
+            </div> 
+            <div className="about-grid-item">
+              <h1>200+</h1>
+              <p>Sessions Completed</p>
+            </div> 
+            <div className="about-grid-item">
+              <h1>20+</h1>
+              <p>Courses Covered</p>
+            </div> 
+            <div className="about-grid-item">
+              <h1>3.95</h1>
+              <p>Average Tutor GPA</p>
+            </div> 
           </div>
         </div>
       </div>
